a7ff160e1d2c9b04878a4ac3b8b3ae839ab94e8a
Diogo Kraut <diogo.kraut@hotmail.com>
2025-03-13 00:55:49 -0300

    feat: create tilesets and add hover behavior

---
+++ Assets/Scripts/GridController.cs
@@ -0,0 +1,64 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class GridController : MonoBehaviour
+{
+    [SerializeField] private Tilemap tilemap;
+    [SerializeField] private Tilemap interactiveTilemap;
+    [SerializeField] private TileBase hoverTile;
+    [SerializeField] private GameObject baseTower;
+
+    private Grid grid;
+    private Vector3Int currentTilePos = new Vector3Int(0, 0, 0);
+    private Dictionary<Vector3Int, GameObject> towers = new Dictionary<Vector3Int, GameObject>();
+    void Start()
+    {
+        grid = GetComponent<Grid>();
+    }
+
+    void Update()
+    {
+        UserMapUpdate();
+    }
+
+    private void UserMapUpdate()
+    {
+        // Hover behaviour
+        Vector3 mouseWorldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector3Int coordinate = GetGridCoordinate(mouseWorldPos);
+        TileBase t = tilemap.GetTile(coordinate);
+        if (coordinate != currentTilePos)
+        {
+            if (t != null)
+            {
+                interactiveTilemap.SetTile(coordinate, hoverTile);
+
+            }
+            interactiveTilemap.SetTile(currentTilePos, null);
+        }
+        currentTilePos = coordinate;
+
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            if(!towers.ContainsKey(coordinate))
+            {
+                towers.Add(coordinate, Instantiate(baseTower));
+                Debug.Log("test");
+                Debug.Log(coordinate);
+            }
+
+            
+        }
+    }
+
+    private Vector3Int GetGridCoordinate(Vector3 position)
+    {
+        Vector3Int coordinate = grid.WorldToCell(position);
+        return new Vector3Int(coordinate.x, coordinate.y, 0);
+    }
+}
---
+++ Assets/Scripts/Tower.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tower : MonoBehaviour
+{
+    public Vector3Int gridPos;
+
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
